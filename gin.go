6a5e183fe3e8589f0788f34853d44891cd00be5d
Supan Adit Pratama <supanadit@gmail.com>
2020-04-10 13:03:51 +0700

    Add gin header authorization for experimental

---
+++ gin.go
@@ -0,0 +1,59 @@
+package ej
+
+import (
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/mitchellh/mapstructure"
+	"strings"
+)
+
+func GetJWTFromGinHeader(c *gin.Context) (string, error) {
+	var token string
+	var err error = nil
+	header := c.GetHeader("Authorization")
+	if header != "" {
+		splitAuthorization := strings.Split(header, " ")
+		if len(splitAuthorization) != 0 && len(splitAuthorization) == 2 {
+			token = splitAuthorization[1]
+		} else {
+			err = errors.New("invalid authorization header")
+		}
+	} else {
+		err = errors.New("no authorization provided")
+	}
+	return token, err
+}
+
+func VerifyGinHeader(c *gin.Context) (claims *Claims, valid bool) {
+	isValid := !IsUseAuthorization()
+	authorization := c.GetHeader("Authorization")
+	if authorization != "" {
+		splitAuthorization := strings.Split(authorization, " ")
+		if len(splitAuthorization) != 0 && len(splitAuthorization) == 2 {
+
+		}
+	}
+	token, err := GetJWTFromGinHeader(c)
+	if err != nil {
+		token, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v \n", token.Header["alg"])
+			}
+			return GetJWTSecretCode(), nil
+		})
+
+		if token != nil {
+			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+				err = mapstructure.Decode(claims, &claims)
+				if err == nil {
+					isValid = true
+				}
+			} else {
+				fmt.Printf("error while parsing token \n")
+			}
+		}
+	}
+	return claims, isValid
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.6.2 // indirect
 	github.com/mitchellh/mapstructure v1.2.2 // indirect
 	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71 // indirect
 )
