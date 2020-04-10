66acb7cce871f4a71b7787a4e8e1fd76bb1b740f
Supan Adit Pratama <supanadit@gmail.com>
2020-04-10 11:16:56 +0700

    Can encrypt custom model

--- authorization.go
+++ authorization.go
@@ -1,16 +1,13 @@
 package ej
 
 import (
-	"fmt"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/mitchellh/mapstructure"
 	"golang.org/x/crypto/bcrypt"
 )
 
 // Default authorization model
 type Authorization struct {
-	Username string `form:"username" json:"username" xml:"username"  binding:"required"`
-	Password string `form:"password" json:"password" xml:"password"  binding:"required"`
+	Username string `form:"username" json:"username" xml:"username" binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
 }
 
 // For JWT purpose
@@ -20,9 +17,7 @@ func (auth Authorization) Valid() error {
 
 // Generate JWT Token by authorization model
 func (auth Authorization) GenerateJWT() (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, auth)
-	tokenString, err := token.SignedString(GetJWTSecretCode())
-	return tokenString, err
+	return GenerateJWT(auth)
 }
 
 // Create password encryption using bcrypt
@@ -36,24 +31,7 @@ func (auth Authorization) GetStringPasswordEncryption() (string, error) {
 	return string(encryption), err
 }
 
-// Verify the authorization
-func (auth Authorization) Verify(authorizationToken string) (bool, error) {
-	isValid := IsUseAuthorization()
-	token, err := jwt.Parse(authorizationToken, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v \n", token.Header["alg"])
-		}
-		return GetJWTSecretCode(), nil
-	})
-	if token != nil {
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			err = mapstructure.Decode(claims, &auth)
-			if err == nil {
-				isValid = true
-			}
-		} else {
-			fmt.Printf("error while parsing token \n")
-		}
-	}
-	return isValid, err
+// VerifyJWT the authorization
+func (auth Authorization) VerifyJWT(token string) (bool, error) {
+	return VerifyJWT(auth, token)
 }
---
+++ custom.go
@@ -0,0 +1,34 @@
+package ej
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/mitchellh/mapstructure"
+)
+
+func GenerateJWT(model jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, model)
+	tokenString, err := token.SignedString(GetJWTSecretCode())
+	return tokenString, err
+}
+
+func VerifyJWT(model jwt.Claims, token string) (bool, error) {
+	isValid := IsUseAuthorization()
+	t, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v \n", token.Header["alg"])
+		}
+		return GetJWTSecretCode(), nil
+	})
+	if t != nil {
+		if claims, ok := t.Claims.(jwt.MapClaims); ok && t.Valid {
+			err = mapstructure.Decode(claims, &model)
+			if err == nil {
+				isValid = true
+			}
+		} else {
+			fmt.Printf("error while parsing t \n")
+		}
+	}
+	return isValid, err
+}
