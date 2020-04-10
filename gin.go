ff980aefe1a8a99ed280ecef1208f2df52a86149
Supan Adit Pratama <supanadit@gmail.com>
2020-04-10 16:50:29 +0700

    Support gin framework

---
+++ gin.go
@@ -0,0 +1,35 @@
+package ej
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+func GetJWTFromGinHeader(c *gin.Context) (token string, err error) {
+	header := c.GetHeader("Authorization")
+	if header != "" {
+		splitAuthorization := strings.Split(header, " ")
+		if len(splitAuthorization) != 0 && len(splitAuthorization) == 2 {
+			token = splitAuthorization[1]
+		} else {
+			err = errors.New("invalid authorization header")
+		}
+	} else {
+		err = errors.New("no authorization provided")
+	}
+	return token, err
+}
+
+func VerifyGinHeader(c *gin.Context) bool {
+	return VerifyAndBindingGinHeader(nil, c)
+}
+
+func VerifyAndBindingGinHeader(model interface{}, c *gin.Context) bool {
+	isValid := !IsUseAuthorization()
+	token, err := GetJWTFromGinHeader(c)
+	if err != nil {
+		isValid, err = VerifyAndBindingJWT(&model, token)
+	}
+	return isValid
+}
