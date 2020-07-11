52ebfd69392fe21652d491dd9ab982f72a85abc8
Supan Adit Pratama <supanadit@gmail.com>
2020-07-11 07:55:06 +0700

    Authorization must provide JWT word for defining type auth

--- web_framework.go
+++ web_framework.go
@@ -10,7 +10,11 @@ func GetJWTFromHeader(header string) (token string, err error) {
 	if header != "" {
 		splitAuthorization := strings.Split(header, " ")
 		if len(splitAuthorization) != 0 && len(splitAuthorization) == 2 {
-			token = splitAuthorization[1]
+			if splitAuthorization[0] != "JWT" {
+				err = errors.New("unknown authorization type")
+			} else {
+				token = splitAuthorization[1]
+			}
 		} else {
 			err = errors.New("invalid authorization header")
 		}
